4802214abbbb9aefb9246984efe50365c909d6d6
anatoly <anatolypolyakovd@gmail.com>
2024-11-09 17:43:11 +1000

    add index_string

--- app.py
+++ app.py
@@ -10,5 +10,30 @@ server = app.server
 
 app.layout = layout
 
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>Карта Снов</title>
+        <meta name="description" content="Карта Снов - интерактивное приложение для работы с географическими координатами, измерения расстояний и магнитного азимута на карте Снов, а также определения номенклатуры соседних листов карты.">
+        <meta name="keywords" content="карта, Снов, карта Снов, географические координаты, прямоугольные координаты, номенклатура, расстояние, магнитный азимут, целеуказание, интерактивное приложение">
+        <meta name="author" content="Борис">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="assets/kompas.png" type="image/png">
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+'''
+
+
 if __name__ == "__main__":
-    app.run_server(debug=False, host='0.0.0.0')
+    app.run_server(host='0.0.0.0')
