7ac1bad1beb76140f939202fe27ab333c90e4939
anatoly <anatolypolyakovd@gmail.com>
2025-04-11 21:28:18 +1000

    fix deploy.sh v.2

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 echo "Остановка текущего контейнера..."
-docker stop karta-snov-container || true
+
+docker stop karta-snov-container 2>/dev/null && docker rm karta-snov-container 2>/dev/null || echo "Контейнер не был запущен."
 
 echo "Выполнение git pull..."
 git pull
 
 echo "Сборка нового образа..."
 docker build -t karta-snov .
-
-docker rm karta-snov-container
 
 echo "Запуск нового контейнера..."
 docker run -d --rm -p 8050:8050 --name karta-snov-container karta-snov
